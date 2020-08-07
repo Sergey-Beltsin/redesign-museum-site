9879cfd978098812916e9c3ed151b7d83f58efdc
Sergey <beltsins89@gmail.com>
2020-08-07 09:01:48 +0300

    Сделали слайдер, доработали новости; продолжаем работу над адаптивом

--- source/js/script.js
+++ source/js/script.js
@@ -1,18 +1,20 @@
 import headMenu from './modules/header-menu';
-// import { tns } from "../../node_modules/tiny-slider/src/tiny-slider";
+import { tns } from "../../node_modules/tiny-slider/src/tiny-slider";
 
 window.addEventListener('DOMContentLoaded', () => {
     headMenu();
 
-//     tns({
-//         container: '.main-slider__slides',
-//         items: 1,
-//         slideBy: 'page',
-//         autoplay: true,
-//         mouseDrag: true,
+    tns({
+        container: '.main-slider__slides',
+        items: 1,
+        slideBy: 'page',
+        autoplay: true,
+        mouseDrag: true,
 
-//         controlsPosition: 'bottom',
-//         controlsText: [' ', ' '],
-//         prevButton: `position: absolute; background: #000000;`
-//       });
-// });
+        controlsPosition: 'bottom',
+        controlsText: [' ', ' ']
+        // prevButton: `position: absolute;`
+
+        // navPosition: 'bottom'
+      });
+});
